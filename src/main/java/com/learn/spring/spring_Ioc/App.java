b12f7ed9cbed907d92f3495a67f644059b30ce04
ramanuj <ramanujk777@gmail.com>
2018-07-22 16:02:32 +0530

    Initial-Commit

---
+++ src/main/java/com/learn/spring/spring_Ioc/App.java
@@ -0,0 +1,13 @@
+package com.learn.spring.spring_Ioc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
